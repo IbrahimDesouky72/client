d67483f1249b54bc9bbc760182b9cd009107e0ce
Ibrahim Desouky <ibrahim.desouky44@gmail.com>
2018-02-18 00:46:18 +0200

    group chat window added

--- src/main/java/com/talktoki/client/controller/GroupChatWindowController.java
+++ src/main/java/com/talktoki/client/controller/GroupChatWindowController.java
@@ -5,17 +5,101 @@
  */
 package com.talktoki.client.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXColorPicker;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import com.talktoki.chatinterfaces.commans.Message;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.TextFlow;
 
 /**
  *
  * @author Mohamed Mahrou
  */
-public class GroupChatWindowController {
+public class GroupChatWindowController implements Initializable{
     private String group_Id;
+    @FXML
+    Circle profileImage;
+    
+    @FXML
+    private VBox messageVBox;
+
+    @FXML
+    private TextField messageTextField;
+
+    @FXML
+    private FontAwesomeIconView fileAttachment;
+
+    @FXML
+    private FontAwesomeIconView sendMessage;
+
+    @FXML
+    private ImageView onlineImage;
+
+    @FXML
+    private FontAwesomeIconView phoneCall;
+
+    @FXML
+    private JFXComboBox<?> fontFamily;
+
+    @FXML
+    private JFXComboBox<?> fontSize;
+
+    @FXML
+    private JFXColorPicker colorPallet;
+
+    @FXML
+    private JFXButton saveButton;
+
+    @FXML
+    private TextFlow usersName;
+
+    @FXML
+    private JFXComboBox<?> fontWeight;
+
+    @FXML
+    private JFXTextField groupName;
+
+    @FXML
+    void attachFile(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveMessages(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendMessage(MouseEvent event) {
+
+    }
 
     public GroupChatWindowController(String group_Id) {
         this.group_Id = group_Id;
     }
     public void receiveFromGroup(String sender_email,Message message){}
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        profileImage.setStroke(Color.SEAGREEN);
+        Image im = new Image("F:\\ITI\\javaProject\\client\\src\\main\\resources\\images\\group.ico",false);
+        profileImage.setFill(new ImagePattern(im));
+        
+    }
 }
